de9c2070a022c220363ca45344c92bf800e7330d
ogolla23 <leonardogolla23@gmail.com>
2024-03-11 22:09:51 -0700

    Task complete

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A module for working with lockboxes.
+"""
+
+from collections import deque
+
+
+def canUnlockAll(boxes):
+    """Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    """
+    n = len(boxes)
+    seen_boxes = [False]*n
+    seen_boxes[0] = True
+    queue = deque(boxes[0])
+    
+    while queue:
+        boxIdx = queue.popleft()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if not seen_boxes[boxIdx]:
+            queue.extend(boxes[boxIdx])
+            seen_boxes[boxIdx] = True
+            
+    return all(seen_boxes)
+
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,2 @@
+Lockboxes task that requires me to write a method to determine if a box can be opened
+by traversing through a list of all boxes.
