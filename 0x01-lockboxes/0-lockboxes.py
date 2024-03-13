a3b713fa3c93118d5d7b6f910007a40f24100eef
ogolla23 <leonardogolla23@gmail.com>
2024-03-12 21:46:38 -0700

    comment update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""A module for working with lockboxes.
-"""
-
+'''A module for working with lockboxes.
+'''
 from collections import deque
 
 
 def canUnlockAll(boxes):
-    """Checks if all the boxes in a list of boxes containing the keys
+    '''Checks if all the boxes in a list of boxes containing the keys
     (indices) to other boxes can be unlocked given that the first
     box is unlocked.
-    """
+    '''
     n = len(boxes)
     seen_boxes = [False]*n
     seen_boxes[0] = True
